f763ceb1f2c2df304f7b29408fdc4ff445ff0d72
Dks <54738768+DIPNEET@users.noreply.github.com>
2020-09-22 09:37:16 +0530

    Update MyHeader.js

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -4,17 +4,6 @@ import { View, Text, StyeSheet ,Alert} from 'react-native';
 
 
 
-const BellIconWithBadge=(props)=>{
-  return(
-    <View>
-      <Icon name='bell' type='font-awesome' color='#696969' size={25}
-        onPress={() =>props.navigation.navigate('Notification')}/>
-       <Badge
-        value="1"
-       containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
-    </View>
-  )
-}
 
 
 const MyHeader = props => {
@@ -22,7 +11,7 @@ const MyHeader = props => {
     <Header
       leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
       centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
-      rightComponent={<BellIconWithBadge {...props}/>}
+     
       backgroundColor = "#eaf8fe"
     />
   );
